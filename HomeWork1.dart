f556dec30fc5eef41b6462b8492ea21078f07ba0
Deyaa-Alwardat <diyaaalwardat259@gmail.com>
2026-01-03 19:12:14 +0300

    Add files via upload

---
+++ HomeWork1.dart
@@ -0,0 +1,15 @@
+/*
+Homework 1
+
+Suppose, your distance to office from home is 25 km and you travel 40 km per hour. 
+Write a program to calculate time taken to reach office in minutes.
+Formula (distance) / (speed)
+*/
+void main(){
+  double  distance = 25;
+  double speed = 40;
+  double timeInHours=distance / speed;
+  double timeInMinutes=timeInHours * 60;
+  print("Time In Hours = ${timeInHours}");
+  print("Time In Minutes = ${timeInMinutes}");
+} 
