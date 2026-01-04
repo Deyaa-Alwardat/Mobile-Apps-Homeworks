91b252afbbf78b01de8020a6fdca2c6a9c88adb7
Deyaa-Alwardat <diyaaalwardat259@gmail.com>
2026-01-04 19:04:10 +0300

    Add files via upload

---
+++ HomeWork3B.dart
@@ -0,0 +1,14 @@
+/*
+  Homework 3B
+
+  • Write a dart program to print 1 to 100 but not 41.
+ */
+void main(){
+  for (int i = 1; i <=100; i++) {
+    if(i == 41){
+      continue;
+    }
+    print(i);
+
+  }
+}
